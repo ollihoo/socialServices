b7df8ac0e2f660c0161038808242a1911b3170e4
Oliver Hoogvliet <oliver@hoogvliet.de>
2024-11-10 12:08:42 +0100

    For my learning of next.js, I add these files

---
+++ nextjs-dashboard/app/dashboard/socialservices/route.ts
@@ -0,0 +1,11 @@
+export const dynamic = 'force-static'
+
+export async function GET() {
+    const res = await fetch('https://localhost:8080/social', {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    const data = await res.json()
+    return Response.json({ data })
+}
