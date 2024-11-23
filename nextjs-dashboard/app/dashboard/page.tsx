a8d279f4dbda4a4cb4bb9d50e62b60734dc235e2
Oliver Hoogvliet <oliver@hoogvliet.de>
2024-11-23 22:58:11 +0100

    SOme more exercise for react stuff

--- nextjs-dashboard/app/dashboard/page.tsx
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { lusitana } from '@/app/ui/fonts';
 import SocialServices from "@/app/ui/dashboard/socialservices";
 import {Suspense} from "react";
 import {RevenueChartSkeleton} from "@/app/ui/skeletons";
+import SearchCategories from "@/app/ui/searchCategories";
 
 export default async function Page() {
     return (
@@ -9,6 +10,9 @@ export default async function Page() {
             <h1 className={`${lusitana.className} antialiased mb-4 text-xl md:text-2xl`}>
                 Dashboard
             </h1>
+          <div>
+            <SearchCategories placeholder="Search categories..." />
+          </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <Suspense fallback={<RevenueChartSkeleton />}>
                     <SocialServices />
