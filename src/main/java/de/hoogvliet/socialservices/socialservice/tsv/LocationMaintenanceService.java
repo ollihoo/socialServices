331f20af9d9d7e4e89eeca06e789f12f85a80fd9
Oliver Hoogvliet <oliver@hoogvliet.de>
2024-11-30 15:00:27 +0100

    [Backend] Extracted Columns into a enum, it increases readability a bit.

--- src/main/java/de/hoogvliet/socialservices/socialservice/tsv/LocationMaintenanceService.java
+++ src/main/java/de/hoogvliet/socialservices/socialservice/tsv/LocationMaintenanceService.java
@@ -12,14 +12,10 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
 
+import static de.hoogvliet.socialservices.socialservice.tsv.TSVDescription.*;
+
 @Service @Log4j2
 public class LocationMaintenanceService {
-    private static final int COLUMN_TIMESTAMP = 0;
-    private static final int COLUMN_NAME = 1;
-    private static final int COLUMN_ADRESS = 2;
-    private static final int COLUMN_POSTCODE = 3;
-    private static final int COLUMN_CITY = 4;
-    private static final int COLUMN_WEBSITE = 5;
     private final LocationRepository locationRepository;
 
     public LocationMaintenanceService(LocationRepository locationRepository) {
@@ -29,27 +25,27 @@ public class LocationMaintenanceService {
     public Location createLocation(String tableReference, String[] columns) {
         Location location = new Location();
         location.setTableReference(tableReference);
-        location.setName(columns[COLUMN_NAME]);
-        location.setAddress(columns[COLUMN_ADRESS]);
-        location.setPostCode(columns[COLUMN_POSTCODE]);
-        location.setCity(columns[COLUMN_CITY]);
+        location.setName(columns[COLUMN_NAME.getColum()]);
+        location.setAddress(columns[COLUMN_ADRESS.getColum()]);
+        location.setPostCode(columns[COLUMN_POSTCODE.getColum()]);
+        location.setCity(columns[COLUMN_CITY.getColum()]);
         location.setWebsite(getWebsite(columns));
         locationRepository.save(location);
         return location;
     }
 
     public Location getOrCreateLocation(String[] columns) {
-        String tableReference = hashString(columns[COLUMN_TIMESTAMP]);
+        String tableReference = hashString(columns[COLUMN_TIMESTAMP.getColum()]);
         Optional<Location> locationOptional = locationRepository.findByTableReference(tableReference);
         return locationOptional.orElseGet(() -> createLocation(tableReference, columns));
     }
 
     private static URL getWebsite(String[] entry) {
-        if (entry.length <= COLUMN_WEBSITE) {
+        if (entry.length <= COLUMN_WEBSITE.getColum()) {
             return null;
         }
         try {
-            URI uri = URI.create(entry[COLUMN_WEBSITE]);//.toURL();
+            URI uri = URI.create(entry[COLUMN_WEBSITE.getColum()]);//.toURL();
             return uri.toURL();
         } catch (IllegalArgumentException | MalformedURLException e) {
             log.warn("Please check tsv input: {}", e.getMessage());
