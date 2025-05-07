346583529512121e9fb83eacc2ac7b66e42fb9e3
Oliver Hoogvliet <oliver@hoogvliet.de>
2025-05-07 14:16:27 +0200

    Remove an unused method in SocialServices

--- src/main/java/de/hoogvliet/socialservices/socialservice/SocialServices.java
+++ src/main/java/de/hoogvliet/socialservices/socialservice/SocialServices.java
@@ -3,18 +3,14 @@ package de.hoogvliet.socialservices.socialservice;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service @Log4j2 @RequiredArgsConstructor
 public class SocialServices {
     private final CategoryRepository categoryRepository;
     private final CityRepository cityRepository;
-    private final LocationRepository locationRepository;
     private final LocationCategoryRepository locationCategoryRepository;
-
-    public List<Location> getAllEntries() {
-        return locationRepository.findAll();
-    }
 
     public List<Category> getCategories() {
         return categoryRepository.findAllByOrderByNameAsc();
