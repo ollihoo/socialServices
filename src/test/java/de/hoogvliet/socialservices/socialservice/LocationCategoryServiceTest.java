b588fca8a17138533e94e2c133472ed239f04ec1
Oliver Hoogvliet <oliver@hoogvliet.de>
2025-07-31 14:19:15 +0200

    Remove stupid crudMethod: step #4 -> more testing

--- src/test/java/de/hoogvliet/socialservices/socialservice/LocationCategoryServiceTest.java
+++ src/test/java/de/hoogvliet/socialservices/socialservice/LocationCategoryServiceTest.java
@@ -2,21 +2,24 @@ package de.hoogvliet.socialservices.socialservice;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class LocationCategoryServiceTest {
+    private static final String ANY_CITY_NAME = "Rotterdam";
+    private static final long ANY_CITY_ID = 59L;
     private final long LOCATION_ID = 2L;
     private final int CATEGORY_ID = 25;
     private final String CATEGORY_NAME = "Beratung";
-
-    private static final String ANY_CITY_NAME = "Rotterdam";
 
     @Mock
     private CityService cityService;
@@ -42,6 +45,30 @@ class LocationCategoryServiceTest {
     }
 
     @Test
+    void updateEntriesWithoutCityId_saves_cities() {
+        List<LocationCategory> lcList = List.of(createLocationCategory());
+        when(locationCategoryRepository.findByCityId(eq(null))).thenReturn(lcList);
+
+        locationCategoryService.updateEntriesWithoutCityId();
+
+        verify(cityService).saveCity(ANY_CITY_NAME);
+    }
+
+    @Test
+    void updateEntriesWithoutCityId_saves_city_in_locationCategories() {
+        City expectedCity = createCity();
+        List<LocationCategory> lcList = List.of(createLocationCategory());
+
+        when(locationCategoryRepository.findByCityId(eq(null))).thenReturn(lcList);
+        when(cityService.saveCity(anyString())).thenReturn(expectedCity);
+        locationCategoryService.updateEntriesWithoutCityId();
+
+        ArgumentCaptor<LocationCategory> captor = ArgumentCaptor.forClass(LocationCategory.class);
+        verify(locationCategoryRepository).save(captor.capture());
+        assertEquals(expectedCity, captor.getValue().getCity());
+    }
+
+    @Test
     void removeOutdatedCategories_same_entry_in_Db_nothing_is_deleted() {
         List<Category> dbCategories = List.of(createCategory(CATEGORY_ID, CATEGORY_NAME));
         List<Category> recentCategories = List.of(createCategory(CATEGORY_ID, CATEGORY_NAME));
@@ -61,7 +88,35 @@ class LocationCategoryServiceTest {
         locationCategoryService.removeOutdatedCategoriesForLocation(location, recentCategories);
         verify(locationCategoryRepository).deleteByCategoryIdAndLocationId(CATEGORY_ID, LOCATION_ID);
     }
+    @Test
+    void addOrUpdateCategories_checks_against_database () {
+        City city = createCity();
+        Location location = createLocation();
+        List<Category> categories = List.of(createCategory(CATEGORY_ID, CATEGORY_NAME));
 
+        when(locationCategoryRepository.findByLocationIdAndCategoryId(anyLong(), anyInt()))
+                .thenReturn(Optional.of(createLocationCategory()));
+
+        locationCategoryService.addOrUpdateCategories(location, categories, city);
+        verify(locationCategoryRepository).findByLocationIdAndCategoryId(LOCATION_ID, CATEGORY_ID);
+    }
+
+    @Test
+    void addOrUpdateCategories_sets_missing_city_entry () {
+        LocationCategory locationCategoryInDb = createLocationCategory(null);
+        City city = createCity();
+        Location location = createLocation();
+        List<Category> categories = List.of(createCategory(CATEGORY_ID, CATEGORY_NAME));
+
+        when(locationCategoryRepository.findByLocationIdAndCategoryId(anyLong(), anyInt()))
+                .thenReturn(Optional.of(locationCategoryInDb));
+
+        locationCategoryService.addOrUpdateCategories(location, categories, city);
+
+        ArgumentCaptor<LocationCategory> captor = ArgumentCaptor.forClass(LocationCategory.class);
+        verify(locationCategoryRepository).save(captor.capture());
+        assertEquals(ANY_CITY_ID, captor.getValue().getCity().getId());
+    }
 
     private Location createLocation() {
         Location location = new Location();
@@ -79,17 +134,22 @@ class LocationCategoryServiceTest {
 
     private City createCity() {
         City city = new City();
-        city.setId(59L);
+        city.setId(ANY_CITY_ID);
         city.setName(ANY_CITY_NAME);
         return city;
     }
 
     private LocationCategory createLocationCategory() {
+        return createLocationCategory(createCity());
+    }
+
+    private LocationCategory createLocationCategory(City city) {
         LocationCategory locationCategory = new LocationCategory();
         locationCategory.setLocation(createLocation());
         locationCategory.setCategory(createCategory(CATEGORY_ID, CATEGORY_NAME));
-        locationCategory.setCity(createCity());
+        locationCategory.setCity(city);
         return locationCategory;
+
     }
 
 }
